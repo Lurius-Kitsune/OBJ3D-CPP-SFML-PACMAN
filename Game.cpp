d9511c49dcc3361f0158aca29174b1be490cb556
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2025-01-08 16:53:11 +0100

    - Game

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,34 @@
 #include "Game.h"
-
+#include "InputManager.h"
 Game::Game()
 {
+	availableLevel =
+	{
+		new Level("SmallMap"),
+		new Level("ClassicMap"),
+		new Level("BigMap")
+	};
+	selectedLevel = availableLevel[0];
+}
+
+Game::~Game()
+{
+	for (Level* _level : availableLevel)
+	{
+		delete _level;
+	}
+}
+
+void Game::Launch()
+{
+	windowGame = RenderWindow(VideoMode(selectedLevel->GetMapSize()), "Pac Man");
+
+	while (windowGame.isOpen())
+	{
+		InputManager::GetInstance().ConsumeInput(windowGame);
+
+		windowGame.clear();
+		selectedLevel->Display(windowGame);
+		windowGame.display();
+	}
 }
